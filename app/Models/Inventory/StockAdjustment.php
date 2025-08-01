b095990e09a7df8fa3c405507cb519ca84fd6a29
elitevinylwindows <aanif@elitevinylwindows.com>
2025-07-31 17:01:05 -0700

    stock adjustment

--- app/Models/Inventory/StockAdjustment.php
+++ app/Models/Inventory/StockAdjustment.php
@@ -3,12 +3,30 @@
 namespace App\Models\Inventory;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Inventory\Product;
+use App\Models\Inventory\StockLevel;
 
 class StockAdjustment extends Model
 {
     protected $table = 'elitevw_inventory_stock_adjustments';
 
     protected $fillable = [
-        'product_id', 'location_id', 'adjustment_type', 'quantity', 'adjusted_by', 'adjusted_date', 'note'
+        'product_id',
+        'location_id',
+        'quantity',
+        'reason',
+        'status',
+        'reference_no',
+        'date'
     ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class, 'location_id');
+    }
 }
