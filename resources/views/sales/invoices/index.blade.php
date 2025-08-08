adadec4e8a47480b52f011b02a3d788abe2f1cec
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-08 10:15:26 -0700

    Update index.blade.php

--- resources/views/sales/invoices/index.blade.php
+++ resources/views/sales/invoices/index.blade.php
@@ -101,10 +101,14 @@
                                 <td class="text-nowrap">
                                     {{-- View --}}
                                     <a class="avtar avtar-xs btn-link-success text-success customModal"
-                                        data-bs-toggle="tooltip" data-bs-original-title="View Invoice" href="#"
-                                        data-size="xl" data-url="{{ route('sales.invoices.show', $invoice->id) }}" data-title="Invoice Summary">
-                                        <i data-feather="eye"></i>
-                                    </a>
+                                           data-bs-toggle="tooltip"
+                                           data-bs-original-title="View Invoice"
+                                           href="#"
+                                           data-size="xl"
+                                           data-url="{{ route('sales.invoices.show', $invoice->id) }}"
+                                           data-title="Invoice Summary">
+                                            <i data-feather="eye"></i>
+</a>
 
                                     {{-- Send to QB --}}
                                     <a class="avtar avtar-xs btn-link-success text-success "
