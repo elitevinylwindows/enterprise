3092a1c837d834927630ac2ed6760fd6cb4bdcde
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-08 12:01:18 -0700

    changes

--- resources/views/master/customers/edit.blade.php
+++ resources/views/master/customers/edit.blade.php
@@ -2,11 +2,7 @@
     @csrf
     @method('PUT')
 
-    <div class="modal-header">
-        <h5 class="modal-title">Edit Customer</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-    </div>
-
+ 
     <div class="modal-body">
         <!-- Basic Information -->
         <div class="row mb-4">
