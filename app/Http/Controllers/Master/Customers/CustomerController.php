b82f59b8744e64a7634fcccf435e4f588933983a
Owais Imam <owais@get-licensed.co.uk>
2025-08-11 23:14:00 +0500

    Update CustomerController.php

--- app/Http/Controllers/Master/Customers/CustomerController.php
+++ app/Http/Controllers/Master/Customers/CustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Master\Customers;
 
+use App\Helper\FirstServe;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Master\Customers\Customer;
