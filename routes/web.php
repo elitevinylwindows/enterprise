4f90e6bd554ab2a2ee4d888ab55fb7f3a78e8f89
Owais Imam <44465478+OwaisImam@users.noreply.github.com>
2025-08-12 18:05:12 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1254,5 +1254,5 @@ Route::get('logs', [\Rap2hpoutre\LaravelLogViewer\LogViewerController::class, 'i
 Route::post('calculate/total', [QuoteController::class, 'calculateTotal'])->name('calculate.total');
 
 Route::group(['prefix' => 'webhooks', 'as' => 'webhooks.'], function () {
-    Route::get('process-invoice', [WebhookController::class, 'handle'])->name('process-invoice');
+    Route::post('process-invoice', [WebhookController::class, 'handle'])->name('process-invoice');
 });
