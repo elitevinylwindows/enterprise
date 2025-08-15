ffd6047ec1619f84abac71ce55cf8571a8a0c865
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-15 15:50:38 -0700

    Update edit.blade.php

--- resources/views/master/series/series_type/edit.blade.php
+++ resources/views/master/series/series_type/edit.blade.php
@@ -2,10 +2,6 @@
   @csrf
   @method('PUT')
 
-  <div class="modal-header">
-    <h5 class="modal-title">{{ __('Edit Series Type') }}</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-  </div>
 
   <div class="modal-body">
     {{-- Series --}}
