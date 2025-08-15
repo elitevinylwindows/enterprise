7fcd6352283d933ae3c5c84bbdc32a2eeb4e747d
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-15 16:02:10 -0700

    Update ProductTypeController.php

--- app/Http/Controllers/Master/ProductKeys/ProductTypeController.php
+++ app/Http/Controllers/Master/ProductKeys/ProductTypeController.php
@@ -10,10 +10,11 @@ use Illuminate\Http\Request;
 class ProducttypeController extends Controller
 {
     public function index()
-    {
-        $items = ProductType::paginate(50);
-        return view('master.product_keys.producttypes.index', compact('items'));
-    }
+{
+    $items  = \App\Models\Master\ProductKeys\ProductType::orderBy('product_type')->get();
+    $series = Series::orderBy('series')->get(); // <-- pass series to view
+    return view('master.product_keys.producttypes.index', compact('items','series'));
+}
 
     public function create()
 {
