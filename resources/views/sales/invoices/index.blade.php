8c913698a20c105a5d060d95a65384700b1f8e82
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-18 14:01:01 -0700

    Update index.blade.php

--- resources/views/sales/invoices/index.blade.php
+++ resources/views/sales/invoices/index.blade.php
@@ -139,6 +139,11 @@
                                         <i data-feather="credit-card"></i>
                                     </a>
 
+                                     {{-- Send Order to Pool without Payment --}}
+                                    <a class="avtar avtar-xs btn-link-success text-success customModal" data-bs-toggle="tooltip" title="Send to Pool (Not Paid)" href="#" data-size="lg" data-url="{{ route('sales.invoices.payment', $invoice->id) }}" data-title="Send to Pool (Not Paid)">
+                                        <i class="fa-solid fa-heart-circle-bolt"></i>
+                                    </a>
+
                                     {{-- Edit --}}
                                     <a class="avtar avtar-xs btn-link-primary text-primary customModal" data-bs-toggle="tooltip" data-bs-original-title="Edit" href="#" data-size="xl" data-url="{{ route('sales.invoices.edit', $invoice->id) }}" data-title="Edit Invoice">
                                         <i data-feather="edit"></i>
