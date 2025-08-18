7a8f5410f1f5d647adfd451f44176f42bb245198
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-18 16:09:19 -0700

    Update create.blade.php

--- resources/views/miscellaneous/parking/create.blade.php
+++ resources/views/miscellaneous/parking/create.blade.php
@@ -1,10 +1,6 @@
 <form method="POST" action="{{ route('misc.parking.store') }}" id="parkingCreateForm">
   @csrf
 
-  <div class="modal-header">
-    <h5 class="modal-title">{{ __('Assign Parking') }}</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('Close') }}"></button>
-  </div>
 
   <div class="modal-body">
     <div class="mb-3">
