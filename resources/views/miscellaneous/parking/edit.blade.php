16eed5015c2a1959a2bee600227d8d9bc609f693
elitevinylwindows <aanif@elitevinylwindows.com>
2025-08-18 16:09:11 -0700

    Update edit.blade.php

--- resources/views/miscellaneous/parking/edit.blade.php
+++ resources/views/miscellaneous/parking/edit.blade.php
@@ -2,11 +2,6 @@
   @csrf
   @method('PUT')
 
-  <div class="modal-header">
-    <h5 class="modal-title">{{ __('Edit Parking') }}</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('Close') }}"></button>
-  </div>
-
   <div class="modal-body">
     <div class="mb-3">
       <label class="form-label">{{ __('User') }}</label>
